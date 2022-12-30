1b6b0b15549c1e3967e667b008bc1db325983355
swifteye1111 <hasak.josh@gmail.com>
2022-12-30 14:51:59 +0100

    Initial commit.

---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game'
+
+# Game Notes:
+# 7 columns, 6 rows
+# Player 1, player 2
+
+describe Game do
+  subject(:new_game) { described_class.new }
+
+  describe '#play_round' do
+
+  end
+end
